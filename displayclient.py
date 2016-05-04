9d6cacf4783a4c53c34ab1521fdea34a357a5ff2
Isaac Smith <isaac@isrv.pw>
2016-05-03 20:00:04 -0500

    Removed old displayclient files

--- displayclient.py
+++
@@ -1,4 +0,0 @@
-from displayclient import DisplayClient
-                    
-if __name__ == '__main__':
-    DisplayClient().run()
--- displayclient/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Fade import Fade
-from .MeteorTime import MeteorTime
-from .Section import Section
-from .DisplaySource import DisplaySource
-from .DisplayClient import DisplayClient
-
-from .MediaAction import MediaAction
